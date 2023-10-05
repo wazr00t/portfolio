09fb4ab484bdf52b1e203d7d0328f396188e6a8d
wazr00t <146912094+wazr00t@users.noreply.github.com>
2023-10-05 08:25:40 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,102 @@
 // src/components/Login.js
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function Login() {
-  // Implement the login page UI here.
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [otp, setOtp] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios.post('/api/login', { email, password, otp });
+
+      if (response.status === 200) {
+        setLoggedIn(true);
+        setMessage('Login successful');
+      }
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.error);
+      } else {
+        setMessage('An error occurred. Please try again later.');
+      }
+    }
+  };
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios.post('/api/register', { email, password });
+
+      if (response.status === 201) {
+        setMessage('Registration successful');
+      }
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.error);
+      } else {
+        setMessage('An error occurred. Please try again later.');
+      }
+    }
+  };
+
   return (
     <div>
-      {/* Google Sign-In Button */}
-      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
-
-      {/* User Registration Form */}
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        {/* Add more input fields for user information */}
-        <button type="submit">Create Account</button>
-      </form>
+      {loggedIn ? (
+        <p>{message}</p>
+      ) : (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password (12+ characters, including uppercase, lowercase, numbers, and special characters)"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="MFA OTP (if enabled)"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+            />
+            <button type="submit">Login</button>
+          </form>
+          <h2>Register</h2>
+          <form onSubmit={handleRegistration}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password (12+ characters, including uppercase, lowercase, numbers, and special characters)"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <p>Other information (e.g., first name, surname) is optional during registration.</p>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
